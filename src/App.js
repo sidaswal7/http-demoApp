8622a720ece394590c6439cac0f51cb4dfd74c31
sidaswal7 <siddhantaswal7@gmail.com>
2024-05-18 11:47:10 +0530

    added error handling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ function App() {
   
   const [movies, setmovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function fetchHandler() {
     setIsLoading(true)
+    setError(null);
+    try{
     const response = await fetch('https://swapi.dev/api/films/');
+    if(!response.ok){
+      throw new Error('Something Went Wrong');
+    }
     const data = await response.json()
     const transMovies = data.results.map(movieData=>{
       return {
@@ -20,8 +26,24 @@ function App() {
       };
     })
     setmovies(transMovies)
+    } catch(error){
+      setError(error.message);
+    }
+    
     setIsLoading(false)
   }
+
+  let content = <p>No Movies Found</p>
+  if(movies.length>0){
+    content = <MoviesList movies={movies}/>;
+  }
+  if(error){
+    content = <p>{error}</p>
+  }
+  if(isLoading){
+    content = <p>Loading page...</p>;
+  }
+
 
   return (
     <React.Fragment>
@@ -29,9 +51,7 @@ function App() {
         <button onClick={fetchHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
-        {isLoading && <p>Page loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
