718f7c5cb674c6c4d1a58ba6393b0762db1d08d4
sidaswal7 <siddhantaswal7@gmail.com>
2024-05-18 09:33:37 +0530

    convert to async

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ import './App.css';
 function App() {
   
   const [movies, setmovies] = useState([]);
-  const fetchHandler = ()=>{
-    fetch('https://swapi.dev/api/films/').then(response=>{
-      return response.json()
-    }).then(data => {
-      const transMovies = data.results.map(movieData=>{
-        return {
-          id:movieData.episode_id,
-          title:movieData.title,
-          releaseDate:movieData.release_date,
-          openingText:movieData.opening_crawl
-        };
-      })
-      setmovies(transMovies)
+  async function fetchHandler() {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json()
+    const transMovies = data.results.map(movieData=>{
+      return {
+        id:movieData.episode_id,
+        title:movieData.title,
+        releaseDate:movieData.release_date,
+        openingText:movieData.opening_crawl
+      };
     })
+    setmovies(transMovies)
   }
 
   return (
